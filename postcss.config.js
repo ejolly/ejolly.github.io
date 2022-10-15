3d643566414cb1ee2f341526a6f4b9072c0e89e0
ejolly <eshin.jolly@gmail.com>
2022-10-14 23:39:42 -0400

    clean up build and add in css minification

--- postcss.config.js
+++ postcss.config.js
@@ -2,8 +2,5 @@ module.exports = {
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
-    ...(process.env.JEKYLL_ENV == 'production'
-      ? [require('cssnano')({ preset: 'default' })]
-      : [])
   ]
 }
